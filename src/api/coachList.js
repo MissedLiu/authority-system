a1f16d8075681334da506c6c48d359f5f08c0c2f
Tian <1508757563@qq.com>
2022-10-08 14:59:51 +0800

    1111

--- src/api/coachList.js
+++ src/api/coachList.js
@@ -1,22 +1,52 @@
 import request from "@/utils/request";
 
-//黑名单会员Api
-
 export default {
-        /**
+  /**
    * 查询教练列表
    * @returns
    */
-         async findCoachList(params) {
-            return await request.get("/api/coach/findCoachList", params);
-          },
-                  /**
+  async findCoachList(params) {
+    return await request.get("/api/coach/findCoachList", params);
+  },
+  /**
    * 修改教练
    * @returns
    */
-         async updateEmp(params) {
-            return await request.put("/api/coach/updateEmp", params);
-          },
-  
-  
-}
+  async updateEmp(params) {
+    return await request.put("/api/coach/updateEmp", params);
+  },
+
+      /**
+   * 查询私教套餐教练关系表数据
+   * @returns
+   */
+       async findPtAndEmp(params) {
+        return await request.get("/api/coach/findPtAndEmp", params);
+      },
+
+    /**
+   * 新增私教套餐教练关系表数据
+   * @returns
+   */
+  async addEmpAndPtMeal(params) {
+    return await request.post("/api/coach/addEmpAndPtMeal", params);
+  },
+
+   /**
+   * 查询团操套餐教练关系表数据
+   * @returns
+   */
+    async findTeamAndEmp(params) {
+      return await request.get("/api/coach/findTeamAndEmp", params);
+    },
+
+  /**
+ * 新增团操套餐教练关系表数据
+ * @returns
+ */
+async addEmpAndTeamMeal(params) {
+  return await request.post("/api/coach/addEmpAndTeamMeal", params);
+},
+
+
+};
